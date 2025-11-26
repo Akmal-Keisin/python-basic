4833e8d6b0fc003aeacb3d45f26e44c07381a8ce
Akmal-Keisin <akmalkeisin@gmail.com>
2025-11-26 16:22:33 +0700

    ADD - new learning

---
+++ 2variable.py
@@ -0,0 +1,21 @@
+# Assigning variable
+hello = "Hello World"
+print(hello)
+
+number = 123
+print (number)
+
+float = 12.34
+print (float)
+
+# Multiple assignment
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+# Change variable value
+a = 10
+print(a)
+a = "Now I'm a string"
+print(a)
